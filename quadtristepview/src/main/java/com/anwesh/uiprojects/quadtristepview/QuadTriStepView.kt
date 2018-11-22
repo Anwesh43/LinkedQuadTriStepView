30e21b79322199c68fa9033d6a40110fd4af3112
Anwesh43 <anweshthecool0@gmail.com>
2018-11-22 10:59:39 +0530

    adding libraries and defining nodes

--- quadtristepview/src/main/java/com/anwesh/uiprojects/quadtristepview/QuadTriStepView.kt
+++ quadtristepview/src/main/java/com/anwesh/uiprojects/quadtristepview/QuadTriStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.quadtristepview
 /**
  * Created by anweshmishra on 22/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Path
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val sizeFactor : Int = 3
+val strokeFactor : Int = 80
