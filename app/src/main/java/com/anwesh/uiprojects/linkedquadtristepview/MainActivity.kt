1a6c16452f02519e245455091bc9306ed0db9267
Anwesh43 <anweshthecool0@gmail.com>
2018-11-22 13:17:21 +0530

    adding View in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedquadtristepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedquadtristepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedquadtristepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.quadtristepview.QuadTriStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : QuadTriStepView = QuadTriStepView.create(this)
     }
 }
